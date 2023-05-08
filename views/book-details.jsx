cc1ed5ced4af27cb2936ed9ab053186a72e7ceb3
roni750 <yerushalmironi@gmail.com>
2023-05-08 14:46:20 +0300

    modified BookDetails cmp, added helpercls flex-row

--- views/book-details.jsx
+++ views/book-details.jsx
@@ -6,11 +6,12 @@ export function BookDetails({ book, onBack }) {
     return (
         <section className="book-details">
             <h1>Book Name: {book.title}</h1>
-            <h5>Price: {book.listPrice.amount}</h5>
+            <h5 class="price">Price: {book.listPrice.amount}</h5>
             <img src={book.thumbnail} alt={book.title}/>
-            {(book.listPrice.isOnSale) ? <h4>On Sale</h4> : ''}
-            {book.pageCount > 500 && <h5>Serious Reading</h5> || book.pageCount > 200 && <h5>Descent Reading</h5> || book.pageCount < 200 && <h5>Light Reading</h5>}
-            {<h5>{estimatedPublishStatus}</h5>}
+            <div className="tags-container flex flex-row">
+            {(book.listPrice.isOnSale) ? <h5 class="tag">On Sale</h5> : ''}
+            <div class="tag">{book.pageCount > 500 && <h5>Serious Reading</h5> || book.pageCount > 200 && <h5>Descent Reading</h5> || book.pageCount < 200 && <h5>Light Reading</h5>}</div>
+            {<h5 class="tag">{estimatedPublishStatus}</h5>}</div>
             <LongTxt bookDescription={book.description} length={90}/>
             {/* <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, velit reiciendis sed optio eum saepe! Aliquid necessitatibus atque est quasi unde odit voluptate! Vero, dolor sunt molestiae possimus labore suscipit?</p> */}
             <button onClick={onBack}>Back</button>
